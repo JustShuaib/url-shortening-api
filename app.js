636c2a6c182081752e42b8aca863ebb766cf97fb
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-05-07 23:11:52 +0100

    Finished styling the mobile nav bar

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelectorAll(".nav-list");
+hamburger.addEventListener("click", (e) => {
+  console.log("You clicked hamburger");
+});
 console.log("Hello");
